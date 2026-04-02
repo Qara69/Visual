9795eae50b40802c4453e9455d69403cf3d246d3
zhurkombaev_daniyar <dddddddddddddzr@mail.ru>
2026-04-02 20:43:56 +0700

    Создал тип PickedByType и написал тест

--- src/task.test.ts
+++ src/task.test.ts
@@ -1,5 +1,5 @@
 import { expectTypeOf, describe, it } from 'vitest';
-import { DeepReadonly} from './task';
+import { DeepReadonly, PickedByType} from './task';
 
 describe('Тесты', () => {
   it('Проверка DeepReadonly', () => {
@@ -7,6 +7,14 @@ describe('Тесты', () => {
     expectTypeOf<DeepReadonly<Obj>>().toEqualTypeOf<{
       readonly a: number;
       readonly b: { readonly c: string };
+    }>();
+  });
+
+  it('Проверка PickedByType', () => {
+    type Mixed = { a: number; b: string; c: number };
+    expectTypeOf<PickedByType<Mixed, number>>().toEqualTypeOf<{
+      a: number;
+      c: number;
     }>();
   });
 });
--- src/task.ts
+++ src/task.ts
@@ -1,3 +1,7 @@
 export type DeepReadonly<T> = {
   readonly [k in keyof T]: T[k] extends object ? DeepReadonly<T[k]> : T[k];
 };
+
+export type PickedByType<T, U> = {
+  [k in keyof T as T[k] extends U ? k : never]: T[k];
+};
