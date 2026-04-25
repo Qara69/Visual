5d580a9a82582588720a8a1ffefbadc232e29ef6
zhurkombaev_daniyar <dddddddddddddzr@mail.ru>
2026-04-25 09:05:02 +0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react';
 
-export default defineConfig ({
+export default defineConfig({
+    plugins: [react()],
     test: {
-        environment: 'node',
-        include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
-        globals: false,
+        environment: 'happy-dom',
+        include: ['src/**/*.test.ts', 'src/**/*.spec.ts', 'src/**/*.test.tsx'],
+        globals: true,
         coverage: {
             provider: 'v8',
             reporter: ['text', 'lcov', 'html'],
